61e84e67b5b6ce37f6c8193532eb1a6548f54270
Omar <omarmata27@gmail.com>
2024-04-04 20:59:18 -0500

    first commit 4/4/24

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-// API Key: https://www.omdbapi.com/?i=tt3896198&apikey=8fc2ef57
-const movieListEl = document.querySelector(".movie__container");
+// API Key: https://www.omdbapi.com/?i=tt3896198&apikey=41e9af4f
+const movieListEl = document.querySelector(".movie__wrapper");
 
-async function onSearchMovie() {
+async function searchMovies() {
   const movies = await fetch(
-    "https://www.omdbapi.com/?i=tt3896198&apikey=8fc2ef57"
+    `https://www.omdbapi.com/?i=tt3896198&apikey=41e9af4f`
   );
   const movieData = await movies.json();
   console.log(movieData);
   movieListEl.innerHTML = movieData
     .map(
-      (movie) => `
-  <div class="movie__wrapper">
-  <figure class="movie__poster">
-  </figure>
-  <h1 class="movie__title"></h1>
-  <p class="movie__runtime">fdsa</p>
-</div>
-        `
+      (movie) =>
+        `<div class="movie__wrapper">
+    <figure class="movie__poster">
+    </figure>
+    <h1 class="movie__title"></h1>
+    <p class="movie__runtime">fdsa</p>
+    </div>
+    `
     )
     .join("");
 }
 
-onSearchMovie();
+searchMovies();
