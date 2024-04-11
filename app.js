3cf2b4135b55a47d7463b3da5117e6a9d615ceca
Omar <omarmata27@gmail.com>
2024-04-10 22:12:54 -0500

    first commit 4/10/24

--- app.js
+++ app.js
@@ -1,45 +1,58 @@
 // API Key: https://www.omdbapi.com/?i=tt3896198&apikey=41e9af4f
-// API Key: http://www.omdbapi.com/?i=tt3896198&apikey=8fc2ef57
-const movieListEl = document.querySelector(".movie__wrapper");
 
-async function searchMovies() {
+const movieListEl = document.getElementById("movieListEl");
+const searchInput = document.getElementById("search__movies__bar");
+
+async function searchMovies(searchTerm) {
   try {
+    // Clear previous search results
+    movieListEl.innerHTML = "";
+
     const movies = await fetch(
-      "http://www.omdbapi.com/?i=tt3896198&apikey=8fc2ef57"
-    ); //response
-    console.log(movies);
+      `https://www.omdbapi.com/?apikey=41e9af4f&s=${searchTerm}`
+    );
     if (!movies.ok) throw new Error("Not a valid response");
-    const movieData = await movies.json(); //object
+    const movieData = await movies.json();
     console.log(movieData);
+    
+    // Check if movies array is empty
+    if (movieData.Search && movieData.Search.length === 0) {
+      throw new Error("No movies found for the given search term");
+    }
+    
+    showMovieInfo(movieData.Search);
   } catch (err) {
     console.warn(err.message);
   }
+}
 
-  movieListEl.innerHTML = movieData
-    .map((movie) => {
-      return `
-      <div class="movie__wrapper">
+function showMovieInfo(movies) {
+  if (!Array.isArray(movies)) {
+    console.warn("No movies found");
+    return;
+  }
+
+  movieListEl.innerHTML = movies.map((movie) => movieInfo(movie)).join("");
+}
+
+function movieInfo(movie) {
+  return `
+    <div class="movie__wrapper">
       <div class="movie__poster">
-        <img src="" alt="" id="poster" />
+        <img src="${movie.Poster}" alt="${movie.Title}" id="poster" />
       </div>
       <div class="movie__info">
-        <h1 class="movie__title">${movie.title}</h1>
-        <p class="movie__year"><b>Released:</b> 2017</p>
-        <p class="movie__actors">
-          <b>Actors:</b> Chris Pratt, Zoe Saldana, Dave Bautista
-        </p>
-        <p class="movie__metascore"><b>Metascore:</b> 67</p>
-        <p class="movie__rated"><b>Rated:</b> PG-13</p>
-        <p class="movie__plot">
-          <b>Plot:</b> "The Guardians struggle to keep together as a team
-          while dealing with their personal family issues, notably
-          Star-Lord's encounter with his father, the ambitious celestial
-          being Ego."
-        </p>
+        <h1 class="movie__title">${movie.Title}</h1>
+        <p class="movie__year"><b>Released:</b> ${movie.Year}</p>
       </div>
     </div>
-`;
-    })
-    .join("");
+  `;
 }
-searchMovies();
+
+searchInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    searchMovies(searchInput.value);
+  }
+});
+
+
