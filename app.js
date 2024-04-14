bb5bff9404bafd35cfe8898611131e8dba140a8f
Omar <omarmata27@gmail.com>
2024-04-14 15:29:17 -0500

    first commit 4/14/24

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-// API Key: https://www.omdbapi.com/?i=tt3896198&apikey=41e9af4f
-
 const movieListEl = document.getElementById("movieListEl");
 const searchInput = document.getElementById("search__movies__bar");
+const searchButton = document.querySelector(".search__btn");
+const homeLink = document.getElementById("homeLink");
+
+// Search when Enter key is pressed in the search input
+searchInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    searchMovies(searchInput.value);
+  }
+});
+
+// Search when search button is clicked
+searchButton.addEventListener("click", function () {
+  searchMovies(searchInput.value);
+});
+
+homeLink.addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent default link behavior
+  resetSearch();
+});
 
 async function searchMovies(searchTerm) {
   try {
+    if (!searchTerm) {
+      console.warn("Search term is empty");
+      return;
+    }
+
     // Clear previous search results
     movieListEl.innerHTML = "";
 
@@ -13,13 +36,12 @@ async function searchMovies(searchTerm) {
     );
     if (!movies.ok) throw new Error("Not a valid response");
     const movieData = await movies.json();
-    console.log(movieData);
-    
+
     // Check if movies array is empty
     if (movieData.Search && movieData.Search.length === 0) {
       throw new Error("No movies found for the given search term");
     }
-    
+
     showMovieInfo(movieData.Search);
   } catch (err) {
     console.warn(err.message);
@@ -39,7 +61,7 @@ function movieInfo(movie) {
   return `
     <div class="movie__wrapper">
       <div class="movie__poster">
-        <img src="${movie.Poster}" alt="${movie.Title}" id="poster" />
+        <img src="${movie.Poster}" alt="${movie.Title}" />
       </div>
       <div class="movie__info">
         <h1 class="movie__title">${movie.Title}</h1>
@@ -49,10 +71,9 @@ function movieInfo(movie) {
   `;
 }
 
-searchInput.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    searchMovies(searchInput.value);
-  }
-});
-
-
+function resetSearch() {
+  // Clear search input
+  searchInput.value = "";
+  // Clear movie posters
+  movieListEl.innerHTML = "";
+}
